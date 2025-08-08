82ce4ff414666b1fd78a3d780cb8b2523f6e32e3
Charlie Kenney <charles.kenney@isovalent.com>
2025-08-08 11:47:48 -0400

    node/manager: prevent MeshNodeSync from pruning cluster (local) nodes
    Fixes an issue in NodeManager where restored cluster nodes can be pruned
    before the initial node listing completes.
    
    Currently, with clustermesh, when all remote clusters have finished
    initial synchronization, `MeshNodeSync` is called, which considers all
    mesh and non-mesh nodes for pruning. If this happens before we finish
    listing initial cluster (local) nodes, we could end up considering the
    restored cluster nodes as stale for pruning. This also causes some
    miscounting for metrics, where for example we decrement `nodes_all_num`
    (in `NodeDeleted` for the synthetic delete event) for nodes we never
    accounted for in the first place.
    
    This patch modifies MeshNodeSync to only consider pruning mesh nodes.

--- pkg/node/manager/manager.go
+++ pkg/node/manager/manager.go
@@ -1034,11 +1034,14 @@ func (m *manager) NodeSync() {
 	})
 }
 
+// MeshNodeSync signals the manager that the initial nodes listing from
+// clustermesh has been completed. This allows the manager to initiate the
+// deletion of possible stale meshed nodes.
 func (m *manager) MeshNodeSync() {
 	m.pruneNodes(true)
 }
 
-func (m *manager) pruneNodes(includeMeshed bool) {
+func (m *manager) pruneNodes(filterMeshed bool) {
 	m.mutex.Lock()
 	if len(m.restoredNodes) == 0 {
 		m.mutex.Unlock()
@@ -1049,28 +1052,33 @@ func (m *manager) pruneNodes(includeMeshed bool) {
 		delete(m.restoredNodes, id)
 	}
 
-	if len(m.restoredNodes) > 0 {
+	toDelete := make([]*nodeTypes.Node, 0, len(m.restoredNodes))
+	for _, n := range m.restoredNodes {
+		if (n.Cluster == m.conf.ClusterName) != filterMeshed {
+			toDelete = append(toDelete, n)
+		}
+	}
+
+	if len(toDelete) > 0 {
 		if log.Logger.IsLevelEnabled(logrus.DebugLevel) {
-			printableNodes := make([]string, 0, len(m.restoredNodes))
-			for ni := range m.restoredNodes {
-				printableNodes = append(printableNodes, ni.String())
+			printableNodes := make([]string, 0, len(toDelete))
+			for _, n := range toDelete {
+				printableNodes = append(printableNodes, n.Identity().String())
 			}
 			log.WithFields(logrus.Fields{
 				"stale-nodes": printableNodes,
-			}).Debugf("Deleting %v stale nodes", len(m.restoredNodes))
+			}).Debugf("Deleting %d stale nodes", len(toDelete))
 		} else {
-			log.Infof("Deleting %v stale nodes", len(m.restoredNodes))
+			log.Infof("Deleting %d stale nodes", len(toDelete))
 		}
 	}
 	m.mutex.Unlock()
 
 	// Delete nodes now considered stale. Can't hold the mutex as
 	// NodeDeleted also acquires it.
-	for id, n := range m.restoredNodes {
-		if n.Cluster == m.conf.ClusterName || includeMeshed {
-			m.NodeDeleted(*n)
-			delete(m.restoredNodes, id)
-		}
+	for _, n := range toDelete {
+		m.NodeDeleted(*n)
+		delete(m.restoredNodes, n.Identity())
 	}
 }
 
